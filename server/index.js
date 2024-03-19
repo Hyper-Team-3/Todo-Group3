f39df198ec7f08134a872f6548f328bec57bdba7
Alexander Heras <alexander.heras@hotmail.com>
2024-03-19 15:56:02 +0100

    Trigger pipeline for server with right env variables

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const swaggerUI = require("swagger-ui-express"); //swagger api
 const swaggerJsDoc = require("swagger-jsdoc"); //swagger api
 
 const options = {
-  //swagger api test
+  //swagger api
   definition: {
     openapi: "3.0.0",
     info: {
