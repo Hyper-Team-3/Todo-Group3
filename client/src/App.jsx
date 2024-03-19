e027a370423f063052adbc9b7306167de76c7d90
Minimoocodes <jaemin.shim14@gmail.com>
2024-03-19 13:37:54 +0100

    router set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter, createBrowserRouter } from "react-router-dom";
+import TodoBoard from "./pages/TodoBoard";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <TodoBoard /> },
+    { path: "/signup", element: <Signup /> },
+    { path: "/login", element: <Login /> },
+  ]);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <BrowserRouter router={router} />
     </>
   );
 }
