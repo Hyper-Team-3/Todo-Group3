cd381ca03da478ba481dda8fc494818686984e20
Pippa <carmela.pippa@hyperisland.se>
2024-03-20 10:59:03 +0100

    Fixed css and html for DoneTask, Footer,  task and TodoBoard

--- client/src/components/TodoBoard/TodoBoard.jsx
+++ client/src/components/TodoBoard/TodoBoard.jsx
@@ -1,10 +1,23 @@
 import Header from "../Header";
-import Sidebar from "../Sidebar";
+import Tasks from "../Tasks";
+import DoneTasks from "../DoneTasks";
+import Footer from "../Footer";
+
+import styles from "./TodoBoard.module.css";
 
 const TodoBoard = () => {
   return (
     <>
-      <Header />
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.tasks_cards}>
+        <Tasks />
+        <DoneTasks />
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
     </>
   );
 };
--- client/src/pages/TodoBoardPage.jsx
+++ client/src/pages/TodoBoardPage.jsx
@@ -1,19 +1,24 @@
 import Sidebar from "../components/Sidebar";
 import TodoBoard from "../components/TodoBoard";
-import Header from "../components/Header";
-import Tasks from "../components/Tasks";
-import DoneTasks from "../components/DoneTasks";
-import Footer from "../components/Footer"
 
 const TodoBoardPage = () => {
   return (
-    <div className="grid grid-cols-13 grid-rows-6 bg-gray-200 overflow-hidden">
+    <div className="grid grid-cols-12 grid-rows-12 gap-2">
+      <div className="row-span-12">
         <Sidebar />
-        <Header />
-        <Tasks />
-        <DoneTasks />
-        <Footer />
+      </div>
+      <div className="col-span-11 row-span-12 flex flex-col">
+        <TodoBoard />
+      </div>
     </div>
+
+    // <div className="grid grid-cols-13 grid-rows-6 bg-gray-200 overflow-hidden">
+    //     <Sidebar />
+    //     <Header />
+    //     <Tasks />
+    //     <DoneTasks />
+    //     <Footer />
+    // </div>
   );
 };
 
