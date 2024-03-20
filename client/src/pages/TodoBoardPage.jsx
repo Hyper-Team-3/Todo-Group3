1d0f05786ac76f31d1ab888a26937eb329bb8e28
Pippa <carmela.pippa@hyperisland.se>
2024-03-20 11:18:58 +0100

    fixed bg

--- client/src/pages/TodoBoardPage.jsx
+++ client/src/pages/TodoBoardPage.jsx
@@ -3,7 +3,7 @@ import TodoBoard from "../components/TodoBoard";
 
 const TodoBoardPage = () => {
   return (
-    <div className="grid grid-cols-12 grid-rows-12 gap-2">
+    <div className="grid grid-cols-12 grid-rows-12 gap-2 bg-gray-200">
       <div className="row-span-12">
         <Sidebar />
       </div>
